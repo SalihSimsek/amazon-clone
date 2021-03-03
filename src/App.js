d0650b6e74ce68ac720f176fcfd6559e9a7d995b
salihsimseek@gmail.com <salihsimseek@gmail.com>
2021-03-03 16:03:03 +0300

    Header(Logo, search, nav) component completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
+import Header from './components/Header'
+
 function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route path="/" exact>
             <h1>Home page</h1>
