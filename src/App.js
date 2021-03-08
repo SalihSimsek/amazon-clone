1513bf20b5036416dc26451d2e279664743a9d7c
salihsimseek@gmail.com <salihsimseek@gmail.com>
2021-03-08 08:50:10 +0300

    Firebase authentication added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
@@ -6,14 +6,41 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Checkout from './components/Checkout'
+import Login from './components/Login'
+import { useStateValue } from "./StateProvider";
+import { auth } from './firebase'
 
 function App() {
+
+  const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+  console.log(user)
+
   return (
     <Router>
       <div className="app">
         <Switch>
           <Route path="/login">
-            <h1>Login</h1>
+            <Login />
           </Route>
           <Route path="/" exact>
             <Header />
@@ -21,7 +48,7 @@ function App() {
           </Route>
           <Route path="/checkout">
             <Header />
-            <Checkout/>
+            <Checkout />
           </Route>
         </Switch>
       </div>
--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,4 @@
-export const getBasketTotal = (basket) => basket?.reduce((amount,item) => item.price + amount,0)
+export const getBasketTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0)
 
 export const initialState = {
     basket: [],
@@ -11,16 +11,20 @@ function reducer(state, action) {
             return {
                 ...state, basket: [...state.basket, action.item]
             }
+        case 'SET_USER':
+            return {
+                ...state, user: action.user
+            }
         case 'REMOVE_FROM_BASKET':
             let newBasket = [...state.basket]
 
             const index = state.basket.findIndex((basketItem) => basketItem.id === action.id)
 
-            if(index >= 0){
-                newBasket.splice(index,1);
+            if (index >= 0) {
+                newBasket.splice(index, 1);
             }
 
-            return  {...state, basket: newBasket}
+            return { ...state, basket: newBasket }
         default:
             return state
     }
